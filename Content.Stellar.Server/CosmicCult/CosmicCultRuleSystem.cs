65705a3ac66bb8319b5e4db381a003fab556a191
Janet Blackquill <uhhadd@gmail.com>
2025-07-21 18:00:00 -0400

    fix cosmic cult build

--- Content.Stellar.Server/CosmicCult/CosmicCultRuleSystem.cs
+++ Content.Stellar.Server/CosmicCult/CosmicCultRuleSystem.cs
@@ -755,7 +755,7 @@ public sealed class CosmicCultRuleSystem : GameRuleSystem<CosmicCultRuleComponen
             return;
         var cosmicGamerule = cult.Comp;
 
-        _stun.TryKnockdown(uid, TimeSpan.FromSeconds(2), true);
+        _stun.TryKnockdown(uid.Owner, TimeSpan.FromSeconds(2), true);
         foreach (var actionEnt in uid.Comp.ActionEntities) _actions.RemoveAction(actionEnt);
 
         if (TryComp<IntrinsicRadioTransmitterComponent>(uid, out var transmitter))
